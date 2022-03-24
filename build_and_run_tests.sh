f3e3dd6ecd00fc9daf714f54540f7ce5f188d14c
Maksim Nesterenko <maksim.nesterenko@unikie.com>
2022-03-24 09:54:33 +0200

    test setup

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -1,7 +1,10 @@
 set -x 
 
+#build
 bazel build --cxxopt='-std=c++14' //src/main:receive_main
 bazel build --cxxopt='-std=c++14' //src/main:send_main
+
+bazel test --cxxopt='-std=c++14' --test_output=all //tests:GoogleTest
 
 bazel clean
 set +x
