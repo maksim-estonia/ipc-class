f036009ec5e5dfbc266fc07e32c1883f29f0aab1
Maksim Nesterenko <maksim.nesterenko@unikie.com>
2022-03-29 11:20:20 +0300

    opening files works

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+- [IPC class](#ipc-class)
+  - [UML graph](#uml-graph)
+  - [xdot graph](#xdot-graph)
+    - [build - receive_main](#build---receive_main)
+    - [build - send_main](#build---send_main)
+    - [test](#test)
+  - [Setup](#setup)
+  - [Build & Run](#build--run)
+  - [Testing](#testing)
+
 # IPC class
 
 A (factory) class-based implementation for conducting IPC using pipe, queue or shared memory.
@@ -6,11 +16,33 @@ A (factory) class-based implementation for conducting IPC using pipe, queue or s
 
 ![uml-graph.jpg](/uml-graph.jpg)
 
+## xdot graph
+
+### build - receive_main
+
+`xdot <(bazel query --notool_deps --noimplicit_deps "deps(//src/main:receive_main)" --output graph)`
+
+![images/build-receive-main.png](/images/build-receive-main.png)
+
+### build - send_main
+
+`xdot <(bazel query --notool_deps --noimplicit_deps "deps(//src/main:send_main)" --output graph)`
+
+![images/build-send-main.png](/images/build-send-main.png)
+
+### test
+
+`xdot <(bazel query --notool_deps --noimplicit_deps "deps(//tests:test_main)" --output graph)`
+
+![images/test-graph.png](/images/test-graph.png)
+
 ## Setup
 
 Execute `./environment.sh`
 
-## Building
+## Build & Run
+
+Execute `./build_and_run.sh`
 
 For building we use Bazel
 
@@ -20,28 +52,30 @@ For building we use Bazel
 bazel run src/main:main
 ```
 
-    - Run (receive)
-    ```
-    bazel run src/main:receive_main
-    ```
-
-    - Run (send)
-    ```
-    bazel run src/main:send_main
-    ```
-
-- Build (test)
-
+- Run (receive)
+  
 ```
-bazel build tests:tests
+bazel run src/main:receive_main
 ```
 
-- Test (test)
-
+- Run (send)
+  
 ```
-bazel test tests:tests
+bazel run src/main:send_main
 ```
 
 ## Testing
 
 For testing we use the GoogleTest framework
+
+- Build (test)
+
+```
+bazel build tests:GoogleTest
+```
+
+- Test (test)
+
+```
+bazel test tests:GoogleTest
+```
--- tests/test_main.cc
+++ tests/test_main.cc
@@ -10,14 +10,16 @@ void run_pipe_rx(void)
 {
     std::cout << "run_pipe_rx" << std::endl;
 
-    char path[] = {"output.txt"};
+    char path[] = {"data/output.txt"};
     std::fstream file;
 
     std::cout << "Launch Pipe Rx" << std::endl;
     CreatorIPC* pipe_rx = new CreatorPipeRx();
     file = pipe_rx->openWriteFile(path);
-    ReceiverIPC* pipe_file_rx = pipe_rx->createIpcRx(&file);
-    std::cout << pipe_file_rx->receive();
+    std::string str1 = "hello";
+    file << str1;
+    // ReceiverIPC* pipe_file_rx = pipe_rx->createIpcRx(&file);
+    // std::cout << pipe_file_rx->receive();
 
     // file.open(path, std::fstream::out | std::fstream::trunc);
 
@@ -26,33 +28,31 @@ void run_pipe_rx(void)
     //     // the file hasn't been opened; error
     //     std::cerr << "ERROR CreatorIPC: write file couldn't be opened" << std::endl;
     // }
-
-    //std::cout << "CreatorIPC: openWriteFile\n";
 }
 
 void run_pipe_tx(void)
 {
     std::cout << "run_pipe_tx" << std::endl;
 
-    char path[] = {"input.txt"};
+    char path[] = {"data/input.txt"};
     std::fstream file;
 
     std::cout << "Launch Pipe Tx" << std::endl;
     CreatorIPC* pipe_tx = new CreatorPipeTx();
     file = pipe_tx->openReadFile(path);
-    SenderIPC* pipe_file_tx = pipe_tx->createIpcTx(&file);
-    std::cout << pipe_file_tx->send();
+    // SenderIPC* pipe_file_tx = pipe_tx->createIpcTx(&file);
+    // std::cout << pipe_file_tx->send();
 }
 
 int run_pipe_test(void)
 {
     std::cout << "----------------------Starting run_pipe_test----------------------" << std::endl;
     std::thread th_rx(run_pipe_rx);
-    //std::thread th_tx(run_pipe_tx);
+    std::thread th_tx(run_pipe_tx);
 
     //wait for both threads to finish
     th_rx.join();
-    //th_tx.join();
+    th_tx.join();
 
     std::cout << "----------------------Ending run_pipe_test----------------------" << std::endl;
     return 0;
