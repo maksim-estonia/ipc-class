3736ad18e4bfbbab46c37935eb48603a7ea91daf
Maksim Nesterenko <maksim.nesterenko@unikie.com>
2022-04-05 11:36:09 +0300

    test-log file output added

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -5,7 +5,7 @@ bazel build --cxxopt='-std=c++14' //src/main:receive_main
 bazel build --cxxopt='-std=c++14' //src/main:send_main
 
 #bazel test --cxxopt='-std=c++14' --linkopt="-pthread" --test_output=all //tests:GoogleTest
-bazel run --cxxopt='-std=c++14' //tests:GoogleTest
+bazel run --cxxopt='-std=c++14' //tests:GoogleTest > test-log.txt
 
 #bazel clean
 set +x
