f47526f2807fec1fb81c36f43a9ba45205506555
Maksim Nesterenko <maksim.nesterenko@unikie.com>
2022-04-07 11:05:35 +0300

    reset pipe

--- src/IPC/PipeRx.cc
+++ src/IPC/PipeRx.cc
@@ -7,74 +7,16 @@ void PipeRx::receive(void) {
     this->fileSizeRx();
 
     this->pipeRx();
-
-    // close pipe
-    close(fd);
-    // close the file
-    file->close();
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
 }
 
 void PipeRx::setupPipeRx(void) {
-    // create a FIFO/named pipe
-    mknod(FIFO_FILE, S_IFIFO|0640, 0);
-
-    // open pipe
-    fd = open(FIFO_FILE, O_RDONLY);
-
-    if (fd == -1) {
-        throw std::runtime_error("Pipe-Rx couldn't be opened");
-    }
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
+    ;
 }
 
 void PipeRx::fileSizeRx(void) {
-    // read data, first data is the length of the text file
-    while (size == 0) {
-        read_bytes = read(fd, readbuf, PIPE_SIZE);
-        size = atoi(readbuf);
-    }
-
-    #if PRINT
-    std::cout << "FILE SIZE: " << size << std::endl;
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
+    ;
 }
 
 void PipeRx::pipeRx(void) {
-    int total_read_bytes = 0;
-    
-    while (1) {
-        read_bytes = read(fd, readbuf, PIPE_SIZE-1);
-        if (read_bytes == -1) {
-            throw std::runtime_error("Pipe-Rx read failed");
-        }
-    
-        total_read_bytes += read_bytes;
-        readbuf[read_bytes] = '\0';
-
-        #if PRINT
-        std::cout << "Received string: " << readbuf << std::endl;
-        std::cout << "--->" << (int)strlen(readbuf) << std::endl;
-        #endif
-
-        // write to file
-        *file << readbuf;
-
-
-        // if all bytes received, we can stop
-        if (total_read_bytes >= size) {
-            break;
-        }
-    }
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
+    ;
 }
--- src/IPC/PipeTx.cc
+++ src/IPC/PipeTx.cc
@@ -7,93 +7,16 @@ void PipeTx::send(void) {
     this->fileSizeTx();
 
     this->pipeTx();
-
-    // close pipe
-    close(this->fd);
-    //close file
-    this->file->close();
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif 
 }
 
 void PipeTx::setupPipeTx(void) {
-    fd = open(FIFO_FILE, O_CREAT|O_WRONLY);
-
-    if (fd == -1) {
-        throw std::runtime_error("Pipe-Tx couldn't be opened");
-    }
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
+    ;
 }
 
 void PipeTx::fileSizeTx(void) {
-    // find file size
-    // set position to begin of file
-    this->file->seekg(0, std::ios::beg);
-    auto begin = this->file->tellg();
-    // set position to end of file
-    this->file->seekg(0, std::ios::end);
-    auto end = this->file->tellg();
-    this->size = (int)(end-begin);
-    // set position back to begin
-    this->file->seekg(0, std::ios::beg);
-
-    #if PRINT
-    std::cout << "  FILE SIZE: " << this->size << std::endl;
-    #endif
-
-    // send file size
-    sprintf(this->readbuf, "%d", this->size);
-    write(this->fd, this->readbuf, strlen(this->readbuf));
-    usleep(100000); // 100ms
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
+    ;
 }
 
 void PipeTx::pipeTx(void) {
-    int n = 0;
-    int write_bytes;
-    
-    while (1) {
-        this->file->read(this->readbuf, sizeof(this->readbuf)-1);
-
-        if (this->file->eof()){
-            // EOF reached, send last string
-            int remaining_bytes = (this->size) - (sizeof(readbuf)-1)*n;
-            readbuf[remaining_bytes] = '\0';
-            #if PRINT
-            std::cout << "Last string: " << readbuf << std::endl;
-            std::cout << "--->" << (int)strlen(readbuf) << std::endl;
-            #endif
-            write_bytes = write(this->fd, this->readbuf, remaining_bytes);
-            if (write_bytes == -1) {
-                throw std::runtime_error("Pipe-Tx write failed");
-            }
-            usleep(100000); // 100ms
-            break;
-        }
-
-        // sending full buffer
-        this->readbuf[sizeof(this->readbuf)-1] = '\0';
-        #if PRINT
-        std::cout << "Read string: " << this->readbuf << std::endl;
-        std::cout << "--->" << (int)strlen(readbuf) << std::endl;
-        #endif
-        write_bytes = write(this->fd, this->readbuf, sizeof(this->readbuf));
-        if (write_bytes == -1) {
-            throw std::runtime_error("Pipe-Tx write failed");
-        }
-        usleep(100000); // 100ms
-        n +=1;
-    }
-
-    #if PRINT
-    std::cout << __PRETTY_FUNCTION__ << " finished" << std::endl;
-    #endif
+    ;
 }
